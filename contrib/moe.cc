baa510af832d901908c415110898c5df7e26183a
Kevin Townsend <kevintownsend4219@gmail.com>
2024-12-16 16:40:39 -0800

    .

---
+++ contrib/moe.cc
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+ool moe_bwt(
+    const char *scr_begin,
+    const char *src_end,
+    char *dst_begin,
+    size_t *key) {
+  constexpr size_t radix = 256;
+}
